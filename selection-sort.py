b41777759759d6e1019c722bdf99f934d61f2151
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-13 07:17:58 +0600

    Selection Sort Better Approach

--- selection-sort.py
+++ selection-sort.py
@@ -1,12 +1,9 @@
 arr = [7, 12, 9, 11, 3]
 n = len(arr)
 for i in range(n-1):
-    j = n-1
-    while j > 0:
-        if arr[j] < arr[j-1]:
-            arr[j], arr[j-1] = arr[j-1], arr[j]
-        j -= 1
+   min_index = i
+   for j in range(i+1, n):
+      if arr[j] < arr[min_index]:
+         min_index = j
+   arr[i], arr[min_index] = arr[min_index], arr[i]
 print("Sorted array is:", arr)
-
-# Time complexity: O(n^2)
-# Space complexity: O(1)
