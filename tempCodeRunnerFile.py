b78bc33206d1c1c4a55bc073409abf3d48f2030c
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-15 12:26:21 +0600

    Practise Selection Sort

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,10 @@
 arr = [7, 12, 9, 11, 3]
 n = len(arr) 
-print(arr)
 for i in range(n-1):
    min_index = i
    j = i+1
-   while j<n:
+   while(j<n):
       if arr[j]<arr[min_index]:
          min_index = j
-      j += 1
-   if min_index != i:
-      arr[i], arr[min_index] = arr[min_index], arr[i]
+   arr[i], arr[min_index] = arr[min_index], arr[i]
 print(arr)
