79658438ae804ad03d1c8acc5fc974ae57478c1b
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-15 11:00:00 +0600

    Bubble sort optimal solution

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,9 +1,11 @@
-arr = [7, 12, 9, 11, 3]
+arr= [2,3,4,5]#arr = [64, 34,25, 12, 22, 22, 11, 90, 5]
 n = len(arr)
-for i in range(n-1):
-    for j in range(n-i-1):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
-
-print("Sorted array is:", arr)
-# Time complexity: O(n^2)
+for i in range(n-1): # case 1: i=0
+    swap = False #swap = False
+    for j in range(n-i-1): # j=0; 1; 2
+        if arr[j]>arr[j+1]: #64>34?; 64>25?; 64>12?;
+            arr[j], arr[j+1] = arr[j+1], arr[j] #swap (64, 34) arr = [34, 64,25, 12, 22, 22, 11, 90, 5]; swap(64,25) arr = [34, 25 ,64, 12, 22, 22, 11, 90, 5]
+            swap = True #swap = True; swap = True
+    if not swap: 
+        break
+print(arr)
